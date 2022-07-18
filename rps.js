f0f0171f2bfed5d721ffa803fdb0973ac45830c2
vdantr <veselin.trifonov.tt@gmail.com>
2022-07-18 13:58:20 +0200

    Add finished game

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+var playerWins, computerWins;
+var playerScore, computerScore;
+
 function getComputerChoice() {
     const lst = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
@@ -5,22 +8,42 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLower();
+    playerSelection = playerSelection.toLowerCase();
+    playerWins = false;
 
-    if (playerSelection === computerSelection) { return 'Tie'; }
+    if (playerSelection === computerSelection) { console.log('Tie'); }
     else if (playerSelection == 'rock') {
-        if (computerSelection === 'paper') { return "You Lose! Paper beats Rock"; }
-        else { return "You Win! Rock beats Scissors"; }
+        if (computerSelection === 'paper') { console.log("You Lose! Paper beats Rock"); computerWins = true; }
+        else { console.log("You Win! Rock beats Scissors"); playerWins = true; }
     }
     else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') { return "You Win! Paper beats Rock"; }
-        else { return "You Lose! Scissors beat Paper"; }
+        if (computerSelection === 'rock') { console.log("You Win! Paper beats Rock"); playerWins = true; }
+        else { console.log("You Lose! Scissors beat Paper"); computerWins = true}
     }
     else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') { return "You Lose! Rock beats Scissors"; }
-        else { return "You Win! Scissors beat Paper"; }
+        if (computerSelection === 'rock') { console.log("You Lose! Rock beats Scissors"); computerWins = true}
+        else { console.log("You Win! Scissors beat Paper"); playerWins = true; }
     }
 }
 
-let playerSelection = prompt("Please enter your selection");
-let computerSelection = getComputerChoice();
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("Please enter your selection");
+        let computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+
+        if (playerWins) { playerScore += 1; }
+        if (computerWins) { computerScore += 1; }
+        playerWins = false;
+        computerWins = false;
+    }
+    if (playerScore === computerScore){return "It's a tie!";}
+    else if (playerScore > computerScore){return "You win, congrats!";}
+    else {return "You lose. Maybe next time :)"}
+}
+
+console.log(game());
